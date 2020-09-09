7d1b8d2c25659414f0827e2e5682653ff887df4a
Danielle Blom <dlblom91@gmail.com>
2020-09-09 17:37:32 -0500

    Complete route and query to add new time to swimmer document per swimmer email

--- server/index.js
+++ server/index.js
@@ -6,10 +6,11 @@ const MongoClient = require('mongodb').MongoClient;
 const PORT = 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/public')))
+app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.json());
 app.use(cors());
 
+// mongodb connection
 var db, swimmersCollection;
 const mongo = MongoClient.connect('mongodb://localhost/swim_tracker', { useNewUrlParser: true, useUnifiedTopology: true })
   .then((client) => {
@@ -24,15 +25,32 @@ app.get(`/swimTimes/:email`, (req, res) => {
   let email = req.params.email;
   swimmersCollection.findOne({ email: email })
   .then(data => {
-      console.log(`Successfully found swimmer times by email: ${email}`);
-      res.status(200).send(data);
+    console.log(`Successfully found swimmer times by email: ${email}`);
+    res.status(200).send(data);
   })
-  .catch(err => console.error(`Failed to get times: ${err}`));
-})
+  .catch(err => console.error(`Failed to get times: ${err}`))
+});
 
 // get times by event
 
 // add a new time to document for the logged in swimmer
+app.post(`/swimTimes/:email`, (req, res) => {
+  let email = req.params.email;
+  let time = req.body.time;
+  let date = req.body.date;
+  let event = req.body.event;
+  swimmersCollection.updateOne(
+    { email: email },
+    {
+      $push: { [event]: { "time": time, "event": event, "date": date} }
+    }
+  )
+  .then(data => {
+    console.log(`Successfully found swimmer times by email: ${email}`);
+    res.status(200).send(data);
+  })
+  .catch(err => console.error(`Failed to get times: ${err}`))
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at ${PORT}`);
