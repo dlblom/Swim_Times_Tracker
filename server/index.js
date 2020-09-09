467d8645adb77de53265d546fbf50e7c55ae2f7c
Danielle Blom <dlblom91@gmail.com>
2020-09-09 13:27:08 -0500

    Start up express server, generate sample data, create database

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/public')))
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}`);
+});
