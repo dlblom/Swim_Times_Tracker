d6445e210780ab7d454d6535707f3cd0cce82eed
Danielle Blom <dlblom91@gmail.com>
2020-09-10 12:01:59 -0500

    Fullstack functionality to get all times by swimmer first and last name

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React,{useState } from 'react';
 import Searchbar from './Searchbar';
+import axios from 'axios';
 
 const App = () => {
+  const [ times, setTimes ] = useState([]);
+  const [ firstName, setFirstName ] = useState('');
+  const [ lastName, setLastName ] = useState('');
+  const [ searchOption, setSearchOption ] = useState('');
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value)
+  }
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value)
+  }
+
+  const handleSearchOptionsChange = (e) => {
+    setSearchOption(e.target.value)
+  }
+
+  const getTimes = (e) => {
+    e.preventDefault();
+    axios.get(`/swimTimes/${firstName} ${lastName}`)
+      .then(results => {
+        console.log(results.data.times)
+        setTimes(results.data.times)
+    })
+    .catch(err => console.log(`Error retrieving times from database: ${err}`))
+  }
+
   return (
     <div>
-      <Searchbar />
+      <Searchbar handleFirstNameChange={handleFirstNameChange} handleLastNameChange={handleLastNameChange} handleSearchOptionsChange={handleSearchOptionsChange} getTimes={getTimes}  />
     </div>
   )
 }
--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -3,27 +3,26 @@ import { Navbar, Nav, DropdownButton, Dropdown, Form, FormControl, Button } from
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSwimmer } from "@fortawesome/free-solid-svg-icons";
 
-const Searchbar = () => {
+const Searchbar = ({ handleFirstNameChange, handleLastNameChange, handleSearchOptionsChange, getTimes}) => {
   return (
     <div>
       <Navbar bg="primary" variant="dark">
         <Navbar.Brand>
           <FontAwesomeIcon icon={faSwimmer} />Swim Times Tracker
         </Navbar.Brand>
-        <Form className="form-inline ml-auto">
-          <Form.Control
+        <Form className="form-inline ml-auto" onSubmit={(e) => getTimes(e)}>
+          <Form.Control onChange={(e)=>handleSearchOptionsChange(e)}
               as="select"
               className="my-1 mr-sm-2"
               id="inlineFormCustomSelectPref"
               custom>
-              <option value="0">Search Times by...</option>
-              <option value="1">All Times</option>
-              <option value="2">Fastest Times by Event</option>
-              <option value="3">Event</option>
+              <option value="Default">Search Times by...</option>
+              <option value="All Times">All Times</option>
+              <option value="Fastest Times by Event">Fastest Times by Event</option>
             </Form.Control>
-          <FormControl type="text" placeholder="Enter First Name" className="mr-sm-2" />
-          <FormControl type="text" placeholder="Enter Last Name" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
+          <FormControl onChange={(e) => handleFirstNameChange(e)} name="firstName" type="text" placeholder="Enter First Name" className="mr-sm-2" required={true} />
+          <FormControl onChange={(e) => handleLastNameChange(e)} name="lastName" type="text" placeholder="Enter Last Name" className="mr-sm-2" required={true} />
+          <Button variant="outline-light" type="submit">Search</Button>
         </Form>
       </Navbar>
     </div>
